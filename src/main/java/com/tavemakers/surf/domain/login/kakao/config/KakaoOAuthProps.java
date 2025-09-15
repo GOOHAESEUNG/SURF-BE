82aa7ede0c3ef812fc8aabc1296a5185634d49c0
LAICME <ehxhfks@naver.com>
2025-09-16 02:38:38 +0900

    [refact]: KakaoOAuthProps Bean Validation 적용 #7

--- src/main/java/com/tavemakers/surf/domain/login/kakao/config/KakaoOAuthProps.java
+++ src/main/java/com/tavemakers/surf/domain/login/kakao/config/KakaoOAuthProps.java
@@ -2,13 +2,16 @@ package com.tavemakers.surf.domain.login.kakao.config;
 
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @ConfigurationProperties(prefix = "kakao")
+@Validated
 public class KakaoOAuthProps {
-    private final String clientId;
-    private final String clientSecret;
-    private final String redirectUri;
+    @NotBlank private final String clientId;
+    private final String clientSecret; // optional
+    @NotBlank private final String redirectUri;
 
     public KakaoOAuthProps(String clientId, String clientSecret, String redirectUri) {
         this.clientId = clientId;
