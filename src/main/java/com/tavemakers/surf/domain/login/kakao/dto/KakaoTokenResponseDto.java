f9fc92148b925862826611b8759368dc2044575e
LAICME <ehxhfks@naver.com>
2025-09-16 02:42:18 +0900

    [refact]: KakaoTokenResponseDto @JsonNaming 적용 #7

--- src/main/java/com/tavemakers/surf/domain/login/kakao/dto/KakaoTokenResponseDto.java
+++ src/main/java/com/tavemakers/surf/domain/login/kakao/dto/KakaoTokenResponseDto.java
@@ -1,32 +1,21 @@
 package com.tavemakers.surf.domain.login.kakao.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 
 /**
  * Kakao OAuth 토큰 응답 DTO (불변 record 타입)
  */
-
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoTokenResponseDto(
 
-        @JsonProperty("access_token")
         String accessToken,
-
-        @JsonProperty("token_type")
         String tokenType,
-
-        @JsonProperty("refresh_token")
         String refreshToken,
-
-        @JsonProperty("expires_in")
         Long expiresIn,
-
         String scope,
-
-        @JsonProperty("refresh_token_expires_in")
         Long refreshTokenExpiresIn,
-
-        @JsonProperty("id_token")
         String idToken // OIDC 켰을 때만 채워짐
 ) {}
