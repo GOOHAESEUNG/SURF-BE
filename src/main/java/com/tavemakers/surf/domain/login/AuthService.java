8a090a17117d5f604d21d776b4740154be8e8719
LAICME <ehxhfks@naver.com>
2025-09-16 02:31:47 +0900

    [refact]: AuthService 제네릭 적용 및 반환 타입 통일 #7

--- src/main/java/com/tavemakers/surf/domain/login/AuthService.java
+++ src/main/java/com/tavemakers/surf/domain/login/AuthService.java
@@ -1,14 +1,17 @@
 package com.tavemakers.surf.domain.login;
 
-/**
+/*
  * 소셜 로그인 공통 인터페이스
  * @param <T> 소셜 로그인 토큰 응답 DTO 타입
  */
 
-public interface AuthService<T> {
-    String buildAuthorizeUrl();   // 인가 URL 만들기
-    T exchangeCodeForToken(String code); // 토큰 교환
-    String getAccessTokenInfo(String accessToken); // 토큰 유효성 확인
-    Object getUserInfo(String accessToken); // 액세스 토큰으로 사용자 정보 조회
+import reactor.core.publisher.Mono;
+import java.util.Map;
 
+public interface AuthService<T, U> {
+    String buildAuthorizeUrl();
+    Mono<T> exchangeCodeForToken(String code);
+    Mono<Map<String, Object>> getAccessTokenInfo(String accessToken); // String → Map
+    Mono<U> getUserInfo(String accessToken);
 }
+
