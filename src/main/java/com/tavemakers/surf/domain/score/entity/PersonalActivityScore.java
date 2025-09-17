2d187e3f3ea04d294c79190a685e944931b2b225
koreaioi <koreaioi@gachon.ac.kr>
2025-09-17 22:14:19 +0900

    feat: PersonalActivityScore 추가 - 개인활동점수 엔티티 #20

---
+++ src/main/java/com/tavemakers/surf/domain/score/entity/PersonalActivityScore.java
@@ -0,0 +1,45 @@
+package com.tavemakers.surf.domain.score.entity;
+
+import com.tavemakers.surf.domain.member.entity.Member;
+import com.tavemakers.surf.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PersonalActivityScore extends BaseEntity implements ScoreComputable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private Integer score;
+
+    @Override
+    public Integer getScore() {
+        return this.score;
+    }
+
+    @Override
+    public Integer updateScore(Integer score) {
+        this.score += score;
+        return this.score;
+    }
+
+    public static PersonalActivityScore create(Member member) {
+        return PersonalActivityScore.builder()
+                .member(member)
+                .score(100) // 기본 점수 100
+                .build();
+    }
+
+}
