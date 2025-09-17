77ff1e1a8246b5514b2e550d96011180acdd46ee
koreaioi <koreaioi@gachon.ac.kr>
2025-09-17 22:13:47 +0900

    feat: ActivityRecord 추가 - 활동 기록 엔티티 #20

---
+++ src/main/java/com/tavemakers/surf/domain/activity/entity/ActivityRecord.java
@@ -0,0 +1,46 @@
+package com.tavemakers.surf.domain.activity.entity;
+
+import com.tavemakers.surf.domain.activity.entity.enums.ActivityCategory;
+import com.tavemakers.surf.domain.activity.entity.enums.ActivityType;
+import com.tavemakers.surf.domain.activity.entity.enums.ScoreType;
+import com.tavemakers.surf.domain.member.entity.Member;
+import com.tavemakers.surf.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ActivityRecord extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private ActivityCategory category; // 대주제
+
+    private ActivityType activityType; // 소주제 (활동 기록 명)
+
+    private ScoreType scoreType; // 상점, 벌점 여부
+
+    private LocalDate activityDate;
+
+    private Long prefixSum; // 누적합
+
+    private Long appliedScore; // 적용 점수
+
+    private boolean isDeleted;
+
+    // TODO 정적 팩토리 메서드
+
+}
