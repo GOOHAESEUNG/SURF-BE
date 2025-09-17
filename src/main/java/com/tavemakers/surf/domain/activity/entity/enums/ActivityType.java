92aff5d73186750075df4e1695821c6d94376895
koreaioi <koreaioi@gachon.ac.kr>
2025-09-17 22:12:53 +0900

    feat: 활동기록 종류 추가 - 벌점 #20

--- src/main/java/com/tavemakers/surf/domain/activity/entity/enums/ActivityType.java
+++ src/main/java/com/tavemakers/surf/domain/activity/entity/enums/ActivityType.java
@@ -21,7 +21,28 @@ public enum ActivityType {
     ENGAGE_SOCIAL_CLUB("소모임 활동", 3, REWARD),
     WRITE_WIL("WIL 작성", 3, REWARD),
     UPLOAD_INSTAGRAM_STORY("인스타그램 스토리 업로드", 3, REWARD),
-    UPLOAD_TAVE_REVIEW("TAVE 활동 후기 업로드", 20, REWARD);
+    UPLOAD_TAVE_REVIEW("TAVE 활동 후기 업로드", 20, REWARD),
+
+    // PENALTY
+    SESSION_ABSENCE("정규 세션 결석", -30,PENALTY),
+    SESSION_TRUANCY("정규 세션 무단 결석", -100, PENALTY),
+    SESSION_LATE_1_TO_10("정규 세션 지각", -10, PENALTY),
+    SESSION_LATE_11_TO_20("정규 세션 지각", -20, PENALTY),
+    SESSION_LATE_21_TO_30("정규 세션 지각", -30, PENALTY),
+
+    STUDY_LATE_5_TO_9("스터디 지각", -5, PENALTY),
+    STUDY_LATE_10_TO_19("스터디 지각", -10, PENALTY),
+    STUDY_LATE_20_TO_29("스터디 지각", -15, PENALTY),
+    STUDY_ABSENCE("스터디 결석", -30, PENALTY),
+
+    PROJECT_LATE_5_TO_9("프로젝트 지각", -5, PENALTY),
+    PROJECT_LATE_10_TO_19("프로젝트 지각", -10, PENALTY),
+    PROJECT_LATE_20_TO_29("프로젝트 지각", -15, PENALTY),
+    PROJECT_ABSENCE("프로젝트 결석", -30, PENALTY),
+
+    NO_VOTE("투표 미참여", -15, PENALTY),
+    DELAY_DEPOSIT("보증금 입금 지연", -5, PENALTY),
+    NO_SHOW_AFTER_PARTY("뒷풀이 불참", -10, PENALTY);
 
     private String displayName;
     private Integer score;
