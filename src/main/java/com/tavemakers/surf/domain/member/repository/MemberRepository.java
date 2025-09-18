3caa9c8f9eb96a9504534ef98efb92bcd7bc7459
koreaioi <koreaioi@gachon.ac.kr>
2025-09-18 15:39:00 +0900

    feat: MemberRepository 추가  #22

---
+++ src/main/java/com/tavemakers/surf/domain/member/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.tavemakers.surf.domain.member.repository;
+
+import com.tavemakers.surf.domain.member.entity.Member;
+import com.tavemakers.surf.domain.member.entity.enums.MemberStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByIdAndStatus(Long memberId, MemberStatus status);
+
+}
