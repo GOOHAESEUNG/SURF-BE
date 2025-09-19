c9a462492d0632864959072bd56cdc3725e3670c
koreaioi <koreaioi@gachon.ac.kr>
2025-09-19 22:30:04 +0900

    refactor: MemberBadgeSaveService 추가 - 활동뱃지 일괄 부여 메서드 #30

---
+++ src/main/java/com/tavemakers/surf/domain/badge/service/MemberBadgeSaveService.java
@@ -0,0 +1,26 @@
+package com.tavemakers.surf.domain.badge.service;
+
+import com.tavemakers.surf.domain.badge.dto.request.MemberBadgeReqDTO;
+import com.tavemakers.surf.domain.badge.entity.MemberBadge;
+import com.tavemakers.surf.domain.badge.repository.MemberBadgeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberBadgeSaveService {
+
+    private final MemberBadgeRepository memberBadgeRepository;
+
+    public void saveMemberBadgeList(MemberBadgeReqDTO dto) {
+        List<MemberBadge> badgeList = new LinkedList<>();
+        dto.memberIdList().forEach(
+                        memberId -> badgeList.add(MemberBadge.of(dto, memberId))
+                );
+        memberBadgeRepository.saveAll(badgeList);
+    }
+
+}
