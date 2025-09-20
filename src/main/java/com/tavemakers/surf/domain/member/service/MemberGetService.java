16a19252fe562e11c063c001f05fc0585b5d3f69
GOOHAESEUNG <amu6675@naver.com>
2025-09-21 01:27:33 +0900

    feat : Memebr,Track GetService에 readOnly=true 추가 #35

--- src/main/java/com/tavemakers/surf/domain/member/service/MemberGetService.java
+++ src/main/java/com/tavemakers/surf/domain/member/service/MemberGetService.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberGetService {
 
     private final MemberRepository memberRepository;
