55df3b74effaf874b25d04605797a745336000bd
GOOHAESEUNG <amu6675@naver.com>
2025-09-20 23:45:58 +0900

    feat : tracknotfound 예외 문구 추가 #27

---
+++ src/main/java/com/tavemakers/surf/domain/member/exception/TrackNotFoundException.java
@@ -0,0 +1,10 @@
+package com.tavemakers.surf.domain.member.exception;
+
+import com.tavemakers.surf.global.common.exception.BaseException;
+
+import static com.tavemakers.surf.domain.member.exception.ErrorMessage.TRACK_NOT_FOUND;
+
+public class TrackNotFoundException extends BaseException {
+
+    public TrackNotFoundException() {super(TRACK_NOT_FOUND.getStatus(), TRACK_NOT_FOUND.getMessage());}
+}
