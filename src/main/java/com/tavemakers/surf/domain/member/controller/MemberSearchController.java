69e2be42fbc413499ef92c681874ab38e5a47f6b
GOOHAESEUNG <amu6675@naver.com>
2025-09-21 01:18:51 +0900

    feat : 트랙 +기수별 회원 반환 API추가 #35

--- src/main/java/com/tavemakers/surf/domain/member/controller/MemberSearchController.java
+++ src/main/java/com/tavemakers/surf/domain/member/controller/MemberSearchController.java
@@ -1,6 +1,7 @@
 package com.tavemakers.surf.domain.member.controller;
 
 import com.tavemakers.surf.domain.member.dto.MemberSearchResDTO;
+import com.tavemakers.surf.domain.member.dto.MemberSimpleResDto;
 import com.tavemakers.surf.domain.member.usecase.MemberUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +20,16 @@ public class MemberSearchController {
 
     private final MemberUsecase memberUsecase;
 
+    //이름 기반 조회
     @GetMapping("/search")
     public ResponseEntity<List<MemberSearchResDTO>> searchMemberByName(@RequestParam String name) {
         return ResponseEntity.ok(memberUsecase.findMemberByNameAndTrack(name));
+    }
+
+    //유저 전체 출력시 트랙+기수별 묶어서 출력
+    @GetMapping("/grouped-by-track")
+    public ResponseEntity<Map<String, List<MemberSimpleResDto>>> getGroupedMembers() {
+        Map<String, List<MemberSimpleResDto>> groupedMembers = memberUsecase.getMembersGroupedByTrack();
+        return ResponseEntity.ok(groupedMembers);
     }
 }
