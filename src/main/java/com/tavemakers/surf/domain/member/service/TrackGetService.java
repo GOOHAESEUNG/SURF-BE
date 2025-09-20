6257ace823f59aa48983fc02bcf6fefe0bed5a1f
GOOHAESEUNG <amu6675@naver.com>
2025-09-20 22:53:00 +0900

    feat: getTrack 트랙 정보 가져오는 로직 #27

---
+++ src/main/java/com/tavemakers/surf/domain/member/service/TrackGetService.java
@@ -0,0 +1,19 @@
+package com.tavemakers.surf.domain.member.service;
+
+import com.tavemakers.surf.domain.member.entity.Track;
+import com.tavemakers.surf.domain.member.repository.TrackRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TrackGetService {
+
+    private final TrackRepository trackRepository;
+
+    public Track getTrack(Long memberId) {
+
+        return trackRepository.findById(memberId).orElse(null);
+
+    }
+}
