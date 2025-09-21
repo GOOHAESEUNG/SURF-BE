e50a643357377e3528a5397b0d718a911b35cb6f
LAICME <ehxhfks@naver.com>
2025-09-21 22:18:46 +0900

    refact: phoneNumber null 입력 방지 #19

--- src/main/java/com/tavemakers/surf/domain/member/dto/request/MemberSignupRequest.java
+++ src/main/java/com/tavemakers/surf/domain/member/dto/request/MemberSignupRequest.java
@@ -18,8 +18,9 @@ public class MemberSignupRequest {
     @NotBlank(message = "이메일은 필수 입력값입니다.")
     private String email;
 
+    @NotBlank(message = "전화번호는 필수 입력값입니다.")
     @Pattern(regexp = "^[0-9]{10,11}$", message = "전화번호 형식이 올바르지 않습니다.")
     private String phoneNumber; // 전화번호를 하이픈 써서 받을지 안 써서 받을지?
 
-    private String profileImageUrl; // 프로필 이미지는 필수로 하는게 나을까요??
+    private String profileImageUrl;
 }
