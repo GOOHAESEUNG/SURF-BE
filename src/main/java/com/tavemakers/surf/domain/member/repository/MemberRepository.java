2ff3e9527cde61ec70bdd37e88a296959137aa88
LAICME <ehxhfks@naver.com>
2025-09-21 17:41:24 +0900

    refact: MemberRepository에 이메일 중복 확인 메서드 추가 #19

--- src/main/java/com/tavemakers/surf/domain/member/repository/MemberRepository.java
+++ src/main/java/com/tavemakers/surf/domain/member/repository/MemberRepository.java
@@ -12,4 +12,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByIdAndStatus(Long memberId, MemberStatus status);
 
+    boolean existsByEmail(String email);
 }
