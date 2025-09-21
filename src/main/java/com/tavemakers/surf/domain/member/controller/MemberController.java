f9a42b507339bc81414f9877865f41a0dd3d837b
LAICME <ehxhfks@naver.com>
2025-09-22 01:48:04 +0900

    feat: MemberController에 회원 승인/거절 API 추가 #38

--- src/main/java/com/tavemakers/surf/domain/member/controller/MemberController.java
+++ src/main/java/com/tavemakers/surf/domain/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.tavemakers.surf.domain.member.dto.request.MemberSignupReqDTO;
 import com.tavemakers.surf.domain.member.dto.response.MemberSignupResDTO;
 import com.tavemakers.surf.domain.member.facade.MemberFacade;
 import com.tavemakers.surf.global.common.response.ApiResponse;
+import com.tavemakers.surf.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import jakarta.validation.Valid;
 public class MemberController {
 
     private final MemberFacade memberFacade;
+    private final MemberService memberService;
 
     @PostMapping("/signup")
     public ApiResponse<MemberSignupResDTO> signup(@Valid @RequestBody MemberSignupReqDTO request) {
@@ -22,6 +24,25 @@ public class MemberController {
                 HttpStatus.CREATED,
                 "회원가입 성공",
                 memberFacade.signup(request)
+        );
+    }
+    @PatchMapping("/{id}/approve")
+    public ApiResponse<Void> approveMember(@PathVariable Long id) {
+        memberService.approveMember(id);
+        return ApiResponse.response(
+                HttpStatus.OK,
+                "승인되었습니다.",
+                null
+        );
+    }
+
+    @PatchMapping("/{id}/reject")
+    public ApiResponse<Void> rejectMember(@PathVariable Long id) {
+        memberService.rejectMember(id);
+        return ApiResponse.response(
+                HttpStatus.OK,
+                "거절되었습니다.",
+                null
         );
     }
 }
