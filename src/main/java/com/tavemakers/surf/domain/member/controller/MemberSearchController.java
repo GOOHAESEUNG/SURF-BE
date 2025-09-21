dd5c60c8ff151c293dd633f1181e31e3df79c178
GOOHAESEUNG <amu6675@naver.com>
2025-09-21 20:35:37 +0900

    feat : member responsemessage 추가 #40

--- src/main/java/com/tavemakers/surf/domain/member/controller/MemberSearchController.java
+++ src/main/java/com/tavemakers/surf/domain/member/controller/MemberSearchController.java
@@ -3,10 +3,12 @@ package com.tavemakers.surf.domain.member.controller;
 import com.tavemakers.surf.domain.member.dto.MemberSearchResDTO;
 import com.tavemakers.surf.domain.member.dto.MemberSimpleResDto;
 import com.tavemakers.surf.domain.member.usecase.MemberUsecase;
+import com.tavemakers.surf.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,9 +31,10 @@ public class MemberSearchController {
             summary = "이름 기반 회원 조회",
             description = "파라미터로 특정 이름을 받아 해당하는 회원 리스트를 반환합니다.")
     @GetMapping("/search")
-    public ResponseEntity<List<MemberSearchResDTO>> searchMemberByName(
+    public ApiResponse<List<MemberSearchResDTO>> searchMemberByName(
             @RequestParam @NotBlank(message = "검색어(name)은 필수입니다.") String name) {
-        return ResponseEntity.ok(memberUsecase.findMemberByNameAndTrack(name));
+
+        return ApiResponse.response(HttpStatus.OK,,memberUsecase.findMemberByNameAndTrack(name));
     }
 
     //유저 전체 출력시 트랙+기수별 묶어서 출력
@@ -39,7 +42,7 @@ public class MemberSearchController {
             summary = "활동 중인 회원 전체 출력시 트랙+기수별로 출력 ",
             description = "활동 중인 회원 전체 출력시 트랙+기수별로 출력")
     @GetMapping("/grouped-by-track")
-    public ResponseEntity<Map<String, List<MemberSimpleResDto>>> getGroupedMembers() {
+    public ApiResponse<Map<String, List<MemberSimpleResDto>>> getGroupedMembers() {
         Map<String, List<MemberSimpleResDto>> groupedMembers = memberUsecase.getMembersGroupedByTrack();
         return ResponseEntity.ok(groupedMembers);
     }
