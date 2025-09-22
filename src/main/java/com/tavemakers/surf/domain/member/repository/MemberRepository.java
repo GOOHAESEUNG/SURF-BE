52e253b0d02be8b5ba99fb755d561054af56797a
LAICME <ehxhfks@naver.com>
2025-09-22 20:22:39 +0900

    feat: MemberRepository에 findByEmailAndStatus 메서드 추가 #38

--- src/main/java/com/tavemakers/surf/domain/member/repository/MemberRepository.java
+++ src/main/java/com/tavemakers/surf/domain/member/repository/MemberRepository.java
@@ -19,4 +19,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     //현재 활동 중 + 특정 이름을 가진 회원 리스트 반환
     List<Member> findByActivityStatusAndName(Boolean activityStatus, String name);
+
+    Optional<Member> findByEmailAndStatus(String email, MemberStatus status);
 }
