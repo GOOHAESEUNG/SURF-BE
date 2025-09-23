d6d522b045ca5631233baeb0f6ba0140856c0a13
LAICME <ehxhfks@naver.com>
2025-09-24 01:43:10 +0900

    feat: Track 엔티티에 생성자 추가 #38

--- src/main/java/com/tavemakers/surf/domain/member/entity/Track.java
+++ src/main/java/com/tavemakers/surf/domain/member/entity/Track.java
@@ -28,4 +28,10 @@ public class Track extends BaseEntity {
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
+    public Track(Integer generation, Part part, Member member) {
+        this.generation = generation;
+        this.part = part;
+        this.member = member;
+    }
+
 }
