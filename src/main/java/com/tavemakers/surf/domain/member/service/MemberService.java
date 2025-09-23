dbad9111897c3d07541c347c1c878c9215e9a199
LAICME <ehxhfks@naver.com>
2025-09-23 12:14:20 +0900

    docs: MemberService 인터페이스 메서드에 ADMIN 전용 주석 추가 #38

--- src/main/java/com/tavemakers/surf/domain/member/service/MemberService.java
+++ src/main/java/com/tavemakers/surf/domain/member/service/MemberService.java
@@ -6,7 +6,13 @@ import com.tavemakers.surf.domain.member.dto.response.MemberSignupResDTO;
 public interface MemberService {
     MemberSignupResDTO signup(MemberSignupReqDTO request);
 
+    /**
+     * 회원 승인 (ADMIN 전용)
+     */
     void approveMember(Long memberId);
 
+    /**
+     * 회원 거절 (ADMIN 전용)
+     */
     void rejectMember(Long memberId);
 }
