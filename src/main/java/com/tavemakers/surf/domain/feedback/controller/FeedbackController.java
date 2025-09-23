6c1a632d7977c5b73c940259e896132041ae2fa9
tjdgus3488 <tjdgus3488@naver.com>
2025-09-23 20:14:01 +0900

    fix: feedbackController 인증 객체 사용한 방식으로 수정

--- src/main/java/com/tavemakers/surf/domain/feedback/controller/FeedbackController.java
+++ src/main/java/com/tavemakers/surf/domain/feedback/controller/FeedbackController.java
@@ -3,6 +3,7 @@ package com.tavemakers.surf.domain.feedback.controller;
 import com.tavemakers.surf.domain.feedback.dto.req.FeedbackCreateReqDTO;
 import com.tavemakers.surf.domain.feedback.dto.res.FeedbackResDTO;
 import com.tavemakers.surf.domain.feedback.service.FeedbackService;
+import com.tavemakers.surf.global.util.SecurityUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,19 +22,20 @@ public class FeedbackController {
 
     private final FeedbackService feedbackService;
 
+    /** 피드백 생성 (로그인 사용자) */
     @PostMapping
     public ResponseEntity<FeedbackResDTO> createFeedback(
-            @Valid @RequestBody FeedbackCreateReqDTO req,
-            @RequestParam Long memberId // 추후 수정
+            @Valid @RequestBody FeedbackCreateReqDTO req
     ) {
+        Long memberId = SecurityUtils.getCurrentMemberId();
         return ResponseEntity.ok(feedbackService.createFeedback(req, memberId));
     }
 
-    // 피드백 조회 (운영진용)
+    /** 피드백 조회 (운영진 전용) */
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROOT','MANAGER','PRESIDENT')")
     public ResponseEntity<Page<FeedbackResDTO>> getFeedbacks(
-            @PageableDefault(size = 20, sort = "createdAt",
-                    direction = Sort.Direction.DESC)
+            @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC)
             Pageable pageable
     ) {
         return ResponseEntity.ok(feedbackService.getFeedbacks(pageable));
