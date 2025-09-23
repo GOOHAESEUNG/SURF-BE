91db7fb5f576f8ff82722aeb88fb1d5ae7d51c4b
GOOHAESEUNG <amu6675@naver.com>
2025-09-23 21:04:15 +0900

    feat : updatePRofile 메소드 추가 #42

--- src/main/java/com/tavemakers/surf/domain/member/service/MemberPatchService.java
+++ src/main/java/com/tavemakers/surf/domain/member/service/MemberPatchService.java
@@ -1,8 +1,12 @@
 package com.tavemakers.surf.domain.member.service;
 
+import com.tavemakers.surf.domain.member.dto.request.ProfileUpdateRequestDto;
+import com.tavemakers.surf.domain.member.entity.Member;
+import com.tavemakers.surf.domain.member.exception.MemberNotFoundException;
 import com.tavemakers.surf.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,6 +14,12 @@ public class MemberPatchService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
+    public void updateProfile(Long memberId, ProfileUpdateRequestDto request) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(MemberNotFoundException::new);
 
+        member.updateProfile(request);
+    }
 
 }
