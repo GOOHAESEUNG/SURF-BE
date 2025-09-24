0af148039abc1749f8050f63f9cd85a2fe2d621b
LAICME <ehxhfks@naver.com>
2025-09-24 22:06:54 +0900

    fix: 회원가입 시 트랙 반영 로직 수정 #38

--- src/main/java/com/tavemakers/surf/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/tavemakers/surf/domain/member/service/MemberServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.tavemakers.surf.domain.member.exception.MemberAlreadyExistsException;
 import com.tavemakers.surf.domain.member.entity.enums.MemberStatus;
 import java.util.Locale;
-import com.tavemakers.surf.domain.member.entity.Track;
 
 @Service
 @RequiredArgsConstructor
@@ -47,12 +46,11 @@ public class MemberServiceImpl implements MemberService {
         member.applySignup(request, normalizedPhone);
 
         // 6) 트랙 정보 반영
-        var tracks = request.getTracks().stream()
-                .map(t -> new Track(t.getGeneration(), t.getPart(), member)) // 생성자 활용
-                .toList();
+        member.getTracks().clear(); // 기존 값 초기화
 
-        member.getTracks().clear();          // 기존 값 초기화 (재가입 시 중복 방지)
-        member.getTracks().addAll(tracks);   // 새로 받은 트랙 추가
+        request.getTracks().forEach(t ->
+                member.addTrack(t.getGeneration(), t.getPart())
+        );
 
         // 6) 응답 변환 (영속 엔티티 → DTO)
         return MemberSignupResDTO.from(member);
