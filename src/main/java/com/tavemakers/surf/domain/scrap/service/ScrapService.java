03388f5b81c3b64d0e1d9c78086f2970ba097582
tjdgus3488 <tjdgus3488@naver.com>
2025-09-24 19:56:16 +0900

    chore: 불필요한 문구 import 처리

--- src/main/java/com/tavemakers/surf/domain/scrap/service/ScrapService.java
+++ src/main/java/com/tavemakers/surf/domain/scrap/service/ScrapService.java
@@ -1,18 +1,16 @@
 package com.tavemakers.surf.domain.scrap.service;
 
 import com.tavemakers.surf.domain.member.entity.Member;
-import com.tavemakers.surf.domain.member.exception.MemberNotFoundException;
 import com.tavemakers.surf.domain.member.repository.MemberRepository;
 import com.tavemakers.surf.domain.post.dto.res.PostResDTO;
 import com.tavemakers.surf.domain.post.entity.Post;
 import com.tavemakers.surf.domain.post.exception.PostNotFoundException;
 import com.tavemakers.surf.domain.post.repository.PostRepository;
-import com.tavemakers.surf.domain.post.service.PostService;
 import com.tavemakers.surf.domain.scrap.entity.Scrap;
-import com.tavemakers.surf.domain.scrap.exception.ScrapNotFoundException;
 import com.tavemakers.surf.domain.scrap.repository.ScrapRepository;
 import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -40,7 +38,7 @@ public class ScrapService {
                 if (postRepository.increaseScrapCount(postId, v) > 0) break;
                 if (i == 2) throw new OptimisticLockException("scrapCount 증가 충돌");
             }
-        } catch (org.springframework.dao.DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             // 이미 스크랩되어 있으면 무시(멱등)
         }
     }
