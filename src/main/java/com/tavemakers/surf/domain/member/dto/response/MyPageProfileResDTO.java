35e85828b1e8669f1eca7a63cdebc1f6c76695db
koreaioi <koreaioi@gachon.ac.kr>
2025-09-29 21:21:50 +0900

    refactor: Conflict 과정에서 손실된 코드 복원 #100

--- src/main/java/com/tavemakers/surf/domain/member/dto/response/MyPageProfileResDTO.java
+++ src/main/java/com/tavemakers/surf/domain/member/dto/response/MyPageProfileResDTO.java
@@ -18,10 +18,10 @@ public record MyPageProfileResDTO(
         List<TrackResDTO> trackList,
         List<CareerResDTO> careerList
 ) {
-    public static MyPageProfileResDTO of(Member member, List<TrackResDTO> trackList, BigDecimal activityScore, List<CareerResDTO> careerList, String phoneNumber) {
+    public static MyPageProfileResDTO of(Member member, List<TrackResDTO> trackList, BigDecimal activityScore, List<CareerResDTO> careerList) {
         return MyPageProfileResDTO.builder()
                 .username(member.getName())
-                .phoneNumber(phoneNumber) // 파라미터로 받은 전화번호 사용
+                .phoneNumber(member.getPhoneNumberPublic() ? member.getPhoneNumber() : null) // 파라미터로 받은 전화번호 사용
                 .email(member.getEmail())
                 .university(member.getUniversity())
                 .graduateSchool(member.getGraduateSchool())
