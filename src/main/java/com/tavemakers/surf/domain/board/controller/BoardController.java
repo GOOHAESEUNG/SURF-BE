949725ab829f87ad972aea30bec86728836c4b5e
tjdgus3488 <tjdgus3488@naver.com>
2025-09-30 18:53:35 +0900

    docs: 게시판 api 스웨거 작성

--- src/main/java/com/tavemakers/surf/domain/board/controller/BoardController.java
+++ src/main/java/com/tavemakers/surf/domain/board/controller/BoardController.java
@@ -23,6 +23,7 @@ import static com.tavemakers.surf.domain.board.controller.ResponseMessage.*;
 public class BoardController {
     private final BoardService boardService;
 
+    @Operation(summary = "게시판 생성", description = "새로운 게시판을 생성합니다.")
     @PostMapping("/v1/admin/boards")
     public ApiResponse<BoardResDTO> createBoard(
             @Valid @RequestBody BoardCreateReqDTO req) {
@@ -30,6 +31,7 @@ public class BoardController {
         return ApiResponse.response(HttpStatus.CREATED, BOARD_CREATED.getMessage(), response);
     }
 
+    @Operation(summary = "게시판 목록 조회", description = "모든 게시판을 조회합니다.")
     @GetMapping("/v1/users/boards")
     public ApiResponse<List<BoardResDTO>> getBoards() {
         List<BoardResDTO> response = boardService.getBoards();
