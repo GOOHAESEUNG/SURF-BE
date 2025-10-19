d5ec0d320b28a35f7097714c02036f624178e5c7
GOOHAESEUNG <amu6675@naver.com>
2025-10-19 17:45:38 +0900

    feat: schedule, imageurl 엔티티 추가 #120

---
+++ src/main/java/com/tavemakers/surf/domain/post/entity/Schedule.java
@@ -0,0 +1,44 @@
+package com.tavemakers.surf.domain.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(nullable = false)
+    private LocalDateTime endAt;
+
+    @Column(nullable = false)
+    private String location;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = true)
+    private Post post;
+}
