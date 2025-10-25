592c69549992ebbdc51747227ba4f117b40e2449
tjdgus3488 <tjdgus3488@naver.com>
2025-10-25 22:32:38 +0900

    feat: BoardType enum 값 추가

--- src/main/java/com/tavemakers/surf/domain/board/entity/BoardType.java
+++ src/main/java/com/tavemakers/surf/domain/board/entity/BoardType.java
@@ -1,6 +1,11 @@
 package com.tavemakers.surf.domain.board.entity;
 
 public enum BoardType {
-    NOTICE // 공지사항
-    // 추후 추가
+    NOTICE,   // 공지사항
+    SESSION,    // 행사
+    ACTIVITY, // 활동
+    PARTNERSHIP, // 제휴
+    RELEASE,  // 릴리스
+    PATCH,    // 패치
+    ETC       // 기타
 }
