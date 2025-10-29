a3db38a858d5137de72424ca1750ec9d14d6e93f
LAICME <ehxhfks@naver.com>
2025-10-30 02:39:14 +0900

    refact: RestClientCongfig 타임아웃 설정 #107

--- src/main/java/com/tavemakers/surf/domain/login/kakao/config/RestClientConfig.java
+++ src/main/java/com/tavemakers/surf/domain/login/kakao/config/RestClientConfig.java
@@ -3,17 +3,25 @@ package com.tavemakers.surf.domain.login.kakao.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean(name = "kakaoAuthRestTemplate")
-    public RestTemplate kakaoAuthRestTemplate() {
-        return new RestTemplate(); // 카카오 인증 서버 요청용
+    public RestTemplate kakaoAuthRestTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(2))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build(); // 카카오 인증 서버 요청용
     }
 
     @Bean(name = "kakaoApiRestTemplate")
-    public RestTemplate kakaoApiRestTemplate() {
-        return new RestTemplate(); // 카카오 사용자 정보 요청용
-    }
+    public RestTemplate kakaoApiRestTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(2))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build(); // 카카오 사용자 정보 요청용
+   }
 }
