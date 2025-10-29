33887be2396c210f13d44944bbdbaf4bd1866a1a
LAICME <ehxhfks@naver.com>
2025-10-30 00:37:07 +0900

    refact: MemberService.signup 메서드 파라미터 축소 #107

--- src/main/java/com/tavemakers/surf/domain/member/service/MemberService.java
+++ src/main/java/com/tavemakers/surf/domain/member/service/MemberService.java
@@ -3,17 +3,22 @@ package com.tavemakers.surf.domain.member.service;
 import com.tavemakers.surf.domain.member.dto.request.MemberSignupReqDTO;
 import com.tavemakers.surf.domain.member.dto.response.MemberSignupResDTO;
 import com.tavemakers.surf.domain.member.entity.Member;
+import com.tavemakers.surf.domain.member.entity.enums.MemberStatus;
 
 public interface MemberService {
-    /**
-     * 회원 승인 (ADMIN 전용)
-     */
+
+    /** 회원 승인 (관리자) */
     void approveMember(Member member);
 
-    /**
-     * 회원 거절 (ADMIN 전용)
-     */
+    /** 회원 거절 (관리자) */
     void rejectMember(Member member);
-    MemberSignupResDTO signup(Member member, MemberSignupReqDTO request, String requestId);
+
+    /** 자체 회원가입 신청 완료 */
+    MemberSignupResDTO signup(Member member, MemberSignupReqDTO request);
+
+    /** 온보딩 필요 여부 확인 */
     Boolean needsOnboarding(Member member);
+
+    /** 회원 상태 조회 */
+    MemberStatus memberStatusCheck(Member member);
 }
