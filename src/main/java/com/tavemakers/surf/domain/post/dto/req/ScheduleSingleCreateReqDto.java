0dc4203776ad2c72b5d15605cc6f812c3f5874e4
GOOHAESEUNG <amu6675@naver.com>
2025-10-30 22:56:54 +0900

    feat:singleCreateDto 생성 #1266

---
+++ src/main/java/com/tavemakers/surf/domain/post/dto/req/ScheduleSingleCreateReqDto.java
@@ -0,0 +1,23 @@
+package com.tavemakers.surf.domain.post.dto.req;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+
+@Schema(description = "일정 제목", example = "만남의 장")
+public class ScheduleSingleCreateReqDto {
+
+    @Schema(description = "일정 제목", example = "만남의 장")
+    private String title;
+
+    @Schema(description = "일정 설명", example = "만남의 장을 할 예정입니다. 준비물은 ~~")
+    private String content;
+
+    @Schema(description = "일정 시작 시간", example = "2025-11-15T14:00:00")
+    private LocalDateTime startAt;
+
+    @Schema(description = "일정 종료 시간", example = "2025-11-15T16:00:00")
+    private LocalDateTime endAt;
+
+    @Schema(description = "일정 장소", example = "세종대학교 광개토대왕관")
+    private String location;
+}
