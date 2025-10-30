1ea5a09c29565c3a4ff8e4028f52edac4441596d
koreaioi <koreaioi@gachon.ac.kr>
2025-10-31 00:55:38 +0900

    feat: S3Controller PreSignedUrl 발급 로직 추가 #125

---
+++ src/main/java/com/tavemakers/surf/global/common/s3/controller/S3Controller.java
@@ -0,0 +1,29 @@
+package com.tavemakers.surf.global.common.s3.controller;
+
+import com.tavemakers.surf.global.common.response.ApiResponse;
+import com.tavemakers.surf.global.common.s3.service.S3Service;
+import com.tavemakers.surf.global.common.s3.dto.PreSignedUrlRequestDto;
+import com.tavemakers.surf.global.common.s3.dto.PreSignedUrlResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class S3Controller {
+
+    private S3Service s3Service;
+
+    @PostMapping("/v1/user/presigned-url")
+    public ApiResponse<List<PreSignedUrlResponse>> generatePutPreSignedUrlList(
+            @RequestBody PreSignedUrlRequestDto dto
+    ) {
+        List<PreSignedUrlResponse> response = s3Service.generatePreSignedUrlList(dto.fileNames());
+        return ApiResponse.response(HttpStatus.OK, "", response);
+    }
+
+}
