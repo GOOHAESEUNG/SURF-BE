b5c7230d95c1ea003b0a54f150cc76fc0f0fd3ad
LAICME <ehxhfks@naver.com>
2025-11-02 16:54:09 +0900

    refact: CommentController 공통 경로 적용 #130

--- src/main/java/com/tavemakers/surf/domain/comment/controller/CommentController.java
+++ src/main/java/com/tavemakers/surf/domain/comment/controller/CommentController.java
@@ -19,14 +19,14 @@ import static com.tavemakers.surf.domain.comment.controller.ResponseMessage.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/v1/user/posts")
 @Tag(name = "댓글", description = "댓글 CRUD 관련 API, 대댓글 1단계 가능")
 public class CommentController {
 
     private final CommentService commentService;
 
     @Operation(summary = "댓글 생성 (루트/대댓글)", description = "parentId가 null이면 루트")
-    @PostMapping("/v1/user/posts/{postId}/comments")
+    @PostMapping("/{postId}/comments")
     public ApiResponse<CommentResDTO> createComment(@PathVariable Long postId,
                                                     @Valid @RequestBody CommentCreateReqDTO req) {
         Long memberId = SecurityUtils.getCurrentMemberId();
@@ -35,14 +35,15 @@ public class CommentController {
     }
 
     @Operation(summary = "댓글 목록 조회 (페이징)", description = "루트 댓글과 대댓글 모두 포함. 페이징 처리")
-    @GetMapping("/v1/user/posts/{postId}/comments")
+    @GetMapping("/{postId}/comments")
     public ApiResponse<Slice<CommentResDTO>> getComments(@PathVariable Long postId, Pageable pageable) {
-        Slice<CommentResDTO> response = commentService.getComments(postId, pageable);
+        Long memberId = SecurityUtils.getCurrentMemberId();
+        Slice<CommentResDTO> response = commentService.getComments(postId, pageable, memberId);
         return ApiResponse.response(HttpStatus.OK, COMMENT_READ.getMessage(), response);
     }
 
     @Operation(summary = "댓글 수정 (내 댓글만)", description = "본인이 작성한 댓글만 수정 가능")
-    @PatchMapping("/v1/user/posts/{postId}/comments/{commentId}")
+    @PatchMapping("/{postId}/comments/{commentId}")
     public ApiResponse<CommentResDTO> updateComment(@PathVariable Long postId,
                                               @PathVariable Long commentId,
                                               @Valid @RequestBody CommentUpdateReqDTO req) {
@@ -52,7 +53,7 @@ public class CommentController {
     }
 
     @Operation(summary = "댓글 삭제 (내 댓글만)", description = "본인이 작성한 댓글만 삭제 가능, 대댓글 존재 시 내용만 삭제(삭제된 댓글입니다 처리)")
-    @DeleteMapping("/v1/user/posts/{postId}/comments/{commentId}")
+    @DeleteMapping("/{postId}/comments/{commentId}")
     public ApiResponse<Void> deleteComment(@PathVariable Long postId,
                                     @PathVariable Long commentId) {
         Long memberId = SecurityUtils.getCurrentMemberId();
