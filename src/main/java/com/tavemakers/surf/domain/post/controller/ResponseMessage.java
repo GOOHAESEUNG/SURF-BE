70c44691f8612273b6c985b5a6133c64b0f4f17c
GOOHAESEUNG <amu6675@naver.com>
2025-11-10 20:18:04 +0900

    feat: 일정 삭제 컨트롤러 추가 #144

--- src/main/java/com/tavemakers/surf/domain/post/controller/ResponseMessage.java
+++ src/main/java/com/tavemakers/surf/domain/post/controller/ResponseMessage.java
@@ -21,7 +21,8 @@ public enum ResponseMessage {
 
     SCHEDULE_CREATED("[일정]이 성공적으로 생성되었습니다"),
     SCHEDULE_UPDATED("[일정]이 성공적으로 수정되었습니다."),
-    SCHEDULE_CALENDAR_READ("[일정]이 성공적으로 월별 조회되었습니다.");
+    SCHEDULE_CALENDAR_READ("[일정]이 성공적으로 월별 조회되었습니다."),
+    SCHEDULE_DELETED("[일정]이 성공적으로 삭제되었습니다.");
 
     private final String message;
 
