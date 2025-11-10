14fc0a4d609b19c8cee21f21b0de4a28b3603c26
GOOHAESEUNG <amu6675@naver.com>
2025-11-10 19:47:06 +0900

    feat: 일정 수정 로직 추가 #144

---
+++ src/main/java/com/tavemakers/surf/domain/post/service/SchedulePatchService.java
@@ -0,0 +1,18 @@
+package com.tavemakers.surf.domain.post.service;
+
+import com.tavemakers.surf.domain.post.dto.req.ScheduleUpdateReqDTO;
+import com.tavemakers.surf.domain.post.entity.Schedule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SchedulePatchService {
+
+    //일정 수정
+    @Transactional
+    public void updateSchedule(Schedule schedule, ScheduleUpdateReqDTO dto) {
+        schedule.updateSchedule(dto);
+    }
+}
