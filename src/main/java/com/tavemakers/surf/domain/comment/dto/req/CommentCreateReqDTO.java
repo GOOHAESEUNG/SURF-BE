3dfd1ef39dad46c27b077d694590db6fce7817e5
LAICME <ehxhfks@naver.com>
2025-11-23 19:39:29 +0900

    feat: 댓글 생성 DTO에 parentId 삭제 및 자동 멘션 처리를 위한 isAutoMention 추가 #151

--- src/main/java/com/tavemakers/surf/domain/comment/dto/req/CommentCreateReqDTO.java
+++ src/main/java/com/tavemakers/surf/domain/comment/dto/req/CommentCreateReqDTO.java
@@ -9,8 +9,14 @@ import java.util.List;
 @Schema(description = "댓글 생성 DTO")
 public record CommentCreateReqDTO(
 
-        @Schema(description = "부모 댓글 ID (루트면 null)", example = "null")
-        Long parentId,
+        @Schema(
+                description = """
+                루트 댓글을 생성할 때는 null,
+                대댓글을 생성할 때는 루트 댓글의 ID를 입력합니다.
+                """,
+                example = "null"
+        )
+        Long rootId, // 해당 값 null 이면 루트 댓글, 값 있으면 대댓글
 
         @Schema(description = "내용", example = "좋은 공지 감사합니다!")
         @NotBlank
@@ -18,5 +24,18 @@ public record CommentCreateReqDTO(
         String content,
 
         @Schema(description = "멘션할 회원 ID 목록", example = "[5]")
-        List<Long> mentionMemberIds
+        List<Long> mentionMemberIds,
+
+        @Schema(
+                description = """
+                자동 멘션 여부.
+                true: 원 댓글을 클릭하여 자동으로 멘션된 경우 (대댓글)
+                false: 사용자가 직접 @입력으로 멘션한 경우 (루트 댓글로 취급)
+                """,
+                example = "false"
+        )
+        Boolean isAutoMention
 ) {}
+
+
+
