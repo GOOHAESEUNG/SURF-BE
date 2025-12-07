d4eaa05c7be955c23e97b7f70ccd8ac5ccf9f477
GOOHAESEUNG <amu6675@naver.com>
2025-12-07 23:07:35 +0900

    refact : notificationResDTO record으로 변경 #168

--- src/main/java/com/tavemakers/surf/domain/notification/dto/res/NotificationResDTO.java
+++ src/main/java/com/tavemakers/surf/domain/notification/dto/res/NotificationResDTO.java
@@ -1,18 +1,13 @@
 package com.tavemakers.surf.domain.notification.dto.res;
 
 import com.tavemakers.surf.domain.notification.entity.NotificationType;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@Builder
-public class NotificationResDTO {
-
-    private Long id;
-    private NotificationType type;
-    private String category;       // ACTIVITY / SCHEDULE
-    private String body;
-    private boolean read;
-    private String createdAt;      // yyyy-MM-dd HH:mm 형태 또는 ISO
-
+public record NotificationResDTO (
+        Long id,
+    NotificationType type,
+    String category,       // ACTIVITY / SCHEDULE
+    String body,
+    boolean read,
+    String createdAt
+){
 }
