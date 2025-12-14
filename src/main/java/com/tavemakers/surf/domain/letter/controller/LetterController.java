ca67fe0504851a01bbb00ce5d0cfac0b11e8a6cb
LAICME <ehxhfks@naver.com>
2025-12-14 14:52:23 +0900

    feat: 쪽지 목록 조회 API 추가 #172

--- src/main/java/com/tavemakers/surf/domain/letter/controller/LetterController.java
+++ src/main/java/com/tavemakers/surf/domain/letter/controller/LetterController.java
@@ -7,31 +7,51 @@ import com.tavemakers.surf.global.common.response.ApiResponse;
 import com.tavemakers.surf.domain.member.entity.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import static com.tavemakers.surf.domain.letter.controller.ResponseMessage.LETTER_CREATED;
+import static com.tavemakers.surf.domain.letter.controller.ResponseMessage.LETTER_SENT_READ;
+
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "쪽지", description = "쪽지 전송 기능에 대한 API 입니다.")
+@Tag(name = "쪽지", description = "회원 간 쪽지 전송 및 조회에 대한 API 입니다.")
 public class LetterController {
 
     private final LetterFacade letterFacade;
 
-    @PostMapping("/v1/user/api/letters")
-    @Operation(summary = "쪽지 생성", description = "로그인한 사용자가 다른 회원에게 쪽지를 전송합니다.")
+    @PostMapping("/v1/user/letters")
+    @Operation(summary = "쪽지 전송", description = "로그인한 사용자가 다른 회원에게 쪽지를 전송합니다.")
     public ApiResponse<LetterResponse> createLetter(
             @AuthenticationPrincipal CustomUserDetails user,
-            @RequestBody LetterCreateRequest request
+            @Valid @RequestBody LetterCreateRequest request
     ) {
-        // senderId는 토큰에서 가져옴
-        Long senderId = user.getId();
-
         // Facade 처리 후 최종 응답 반환
-        LetterResponse response = letterFacade.createLetter(senderId, request);
+        LetterResponse response = letterFacade.createLetter(user.getId(), request);
 
-        return ApiResponse.response(HttpStatus.OK, "쪽지 전송 성공", response);
+        return ApiResponse.response(HttpStatus.OK, LETTER_CREATED.getMessage(), response);
+    }
+
+    @GetMapping("/v1/user/letters/sent")
+    @Operation(summary = "쪽지 조회", description = "자신이 보낸 쪽지 목록을 확인합니다.")
+    public ApiResponse<Slice<LetterResponse>> getSentLetters(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(
+                page, size, Sort.by(Sort.Direction.DESC, "createdAt")
+        );
+
+        return ApiResponse.response(
+                HttpStatus.OK,
+                LETTER_SENT_READ.getMessage(),
+                letterFacade.getSentLetters(user.getId(), pageable)
+        );
     }
 
 }
