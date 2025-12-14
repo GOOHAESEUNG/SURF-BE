5ff7b483cc3761009d7a17358d80b5fa0dd7d12c
LAICME <ehxhfks@naver.com>
2025-12-14 14:49:53 +0900

    refact: 저장 및 전송 로직 순서 변경 #172

--- src/main/java/com/tavemakers/surf/domain/letter/facade/LetterFacade.java
+++ src/main/java/com/tavemakers/surf/domain/letter/facade/LetterFacade.java
@@ -3,11 +3,15 @@ package com.tavemakers.surf.domain.letter.facade;
 import com.tavemakers.surf.domain.letter.dto.req.LetterCreateRequest;
 import com.tavemakers.surf.domain.letter.dto.res.LetterResponse;
 import com.tavemakers.surf.domain.letter.entity.Letter;
+import com.tavemakers.surf.domain.letter.exception.LetterMailSendFailException;
+import com.tavemakers.surf.domain.letter.service.LetterGetService;
 import com.tavemakers.surf.domain.letter.service.LetterSaveService;
 import com.tavemakers.surf.domain.member.entity.Member;
 import com.tavemakers.surf.domain.member.service.MemberGetService;
 import com.tavemakers.surf.global.util.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +21,7 @@ public class LetterFacade {
     private final MemberGetService memberGetService;
     private final LetterSaveService letterSaveService;
     private final EmailService emailService;
+    private final LetterGetService letterGetService;
 
     public LetterResponse createLetter(Long senderId, LetterCreateRequest req) {
 
@@ -26,19 +31,7 @@ public class LetterFacade {
         // 2) 수신자 조회
         Member receiver = memberGetService.getMember(req.getReceiverId());
 
-        // 3) 엔티티 생성
-        Letter letter = Letter.create(
-                req.getTitle(),
-                req.getContent(),
-                req.getSns(),
-                req.getReplyEmail(),
-                sender,
-                receiver
-        );
-
-        // 4) 저장
-        Letter saved = letterSaveService.save(letter);
-
+        // 3) 이메일 본문 생성
         String emailBody = """
         [Surf에서 %s님이 보낸 쪽지입니다.]
         
@@ -49,22 +42,39 @@ public class LetterFacade {
         """
                 .formatted(
                         sender.getName(),
-                        saved.getContent(),
-                        saved.getReplyEmail(),
-                        saved.getSns() != null ? saved.getSns() : "-"
+                        req.getContent(),
+                        req.getReplyEmail(),
+                        req.getSns() != null ? req.getSns() : "-"
                 );
 
+        // 4) 이메일 전송 (실패 시 예외)
+        try {
+            emailService.sendMail(
+                    receiver.getEmail(),
+                    req.getTitle(),
+                    emailBody
+            );
+        } catch (Exception e) {
+            throw new LetterMailSendFailException();
+        }
 
-        // *** 이메일 전송 (제목 = title, 본문 = content 그대로 사용) ***
-        emailService.sendMail(
-                receiver.getEmail(),
-                saved.getTitle(),
-                emailBody
+
+        // 5) 엔티티 생성
+        Letter letter = Letter.create(
+                req.getTitle(),
+                req.getContent(),
+                req.getSns(),
+                req.getReplyEmail(),
+                sender,
+                receiver
         );
 
-        // 5) 저장된 엔티티 기반으로 Response 생성
+        // 6) 저장
+        Letter saved = letterSaveService.save(letter);
+
+        // 7) 저장된 엔티티 기반으로 Response 생성
         return LetterResponse.builder()
-                .noteId(saved.getNoteId())
+                .letterId(saved.getNoteId())
                 .title(saved.getTitle())
                 .content(saved.getContent())
                 .sns(saved.getSns())
@@ -76,4 +86,10 @@ public class LetterFacade {
                 .createdAt(saved.getCreatedAt())
                 .build();
     }
+
+    public Slice<LetterResponse> getSentLetters(Long senderId, Pageable pageable) {
+        return letterGetService.getSentLetters(senderId, pageable)
+                .map(LetterResponse::from);
+    }
+
 }
