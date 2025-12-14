c0a29d483647232983fae9ca231287bd5d37fb62
LAICME <ehxhfks@naver.com>
2025-12-14 14:55:29 +0900

    chore: EmailService 발신자(from) 설정 방식 수정 #172

--- src/main/java/com/tavemakers/surf/global/util/EmailService.java
+++ src/main/java/com/tavemakers/surf/global/util/EmailService.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     private final JavaMailSender mailSender;
+    @org.springframework.beans.factory.annotation.Value("${spring.mail.username}")
+    private String from;
 
     public void sendMail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
         message.setTo(to);        // 수신자 이메일
         message.setSubject(subject);
         message.setText(text);
