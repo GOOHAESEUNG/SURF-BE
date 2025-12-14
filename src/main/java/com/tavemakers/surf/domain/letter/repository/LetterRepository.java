e27d490f92bbb5dd971bac93c149655144da538a
LAICME <ehxhfks@naver.com>
2025-12-14 14:45:45 +0900

    feat: 보낸 쪽지 조회를 위한 Repository 메서드 추가 #172

--- src/main/java/com/tavemakers/surf/domain/letter/repository/LetterRepository.java
+++ src/main/java/com/tavemakers/surf/domain/letter/repository/LetterRepository.java
@@ -1,7 +1,10 @@
 package com.tavemakers.surf.domain.letter.repository;
 
 import com.tavemakers.surf.domain.letter.entity.Letter;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
+    Slice<Letter> findBySenderId(Long senderId, Pageable pageable);
 }
