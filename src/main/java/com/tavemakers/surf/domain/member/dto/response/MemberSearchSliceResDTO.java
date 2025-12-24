f4b28f737e3240ddd6272b7964e399a3c381e353
koreaioi <koreaioi@gachon.ac.kr>
2025-12-25 02:07:44 +0900

    feat: 회원검색 0번째 페이지에만 검색조건에 따른 전체 회원수가 나오도록 필드추가

--- src/main/java/com/tavemakers/surf/domain/member/dto/response/MemberSearchSliceResDTO.java
+++ src/main/java/com/tavemakers/surf/domain/member/dto/response/MemberSearchSliceResDTO.java
@@ -8,14 +8,16 @@ import java.util.List;
 @Builder
 public record MemberSearchSliceResDTO(
         List<MemberSearchDetailResDTO> content,
+        Long totalCount,
         int pageNumber,
         int pageSize,
         int numberOfElements,
         boolean isLast
 ) {
-    public static MemberSearchSliceResDTO from(Slice<MemberSearchDetailResDTO> slice) {
+    public static MemberSearchSliceResDTO of(Slice<MemberSearchDetailResDTO> slice, Long totalCount) {
         return MemberSearchSliceResDTO.builder()
                 .content(slice.getContent())
+                .totalCount(totalCount)
                 .pageNumber(slice.getNumber())
                 .pageSize(slice.getSize())
                 .numberOfElements(slice.getNumberOfElements())
