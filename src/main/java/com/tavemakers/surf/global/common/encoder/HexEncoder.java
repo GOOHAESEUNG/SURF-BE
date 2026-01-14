1e4944f421fe962514437382075bf91516936feb
koreaioi <koreaioi@gachon.ac.kr>
2026-01-15 00:28:41 +0900

    feat: HexEncoder 추가

---
+++ src/main/java/com/tavemakers/surf/global/common/encoder/HexEncoder.java
@@ -0,0 +1,17 @@
+package com.tavemakers.surf.global.common.encoder;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class HexEncoder {
+
+    public static String convertHex(byte[] rawHmac) {
+        StringBuilder sb = new StringBuilder();
+        for (byte byteData : rawHmac) {
+            sb.append(String.format("%02x", byteData));
+        }
+        return sb.toString();
+    }
+
+}
