d7bf882fc3bdf5f2b44a047876b58aadfb5e93d7
koreaioi <koreaioi@gachon.ac.kr>
2026-01-18 22:08:48 +0900

    feat: 관리자페이지 회원 상세 조회 API 추가

--- src/main/java/com/tavemakers/surf/domain/member/controller/AdminController.java
+++ src/main/java/com/tavemakers/surf/domain/member/controller/AdminController.java
@@ -1,9 +1,11 @@
 package com.tavemakers.surf.domain.member.controller;
 
+import com.google.protobuf.Api;
 import com.tavemakers.surf.domain.member.dto.request.AdminPageLoginReqDto;
 import com.tavemakers.surf.domain.member.dto.request.PasswordReqDto;
 import com.tavemakers.surf.domain.member.dto.request.RoleChangeRequestDto;
 import com.tavemakers.surf.domain.member.dto.response.AdminPageLoginResDto;
+import com.tavemakers.surf.domain.member.dto.response.MemberInformationResDTO;
 import com.tavemakers.surf.domain.member.usecase.MemberAdminUsecase;
 import io.swagger.v3.oas.annotations.Operation;
 import com.tavemakers.surf.global.common.response.ApiResponse;
@@ -14,8 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import static com.tavemakers.surf.domain.member.controller.ResponseMessage.ADMIN_PAGE_LOGIN_SUCCESS;
-import static com.tavemakers.surf.domain.member.controller.ResponseMessage.MANAGER_PASSWORD_SET_UP_SUCCESS;
+import static com.tavemakers.surf.domain.member.controller.ResponseMessage.*;
 
 @Tag(name = "관리자", description = "관리자용 API")
 @RestController
@@ -50,6 +51,15 @@ public class AdminController {
     ) {
         AdminPageLoginResDto data = memberAdminUsecase.loginAdminHomePage(dto, response);
         return ApiResponse.response(HttpStatus.OK, ADMIN_PAGE_LOGIN_SUCCESS.getMessage(),data);
+    }
+
+    @Operation(summary = "유저 정보 상세 조회", description = "관리자 페이지에서 유저 정보를 상세 조회합니다.")
+    @GetMapping("/v1/manager/member/{memberId}")
+    public ApiResponse<MemberInformationResDTO> readMemberInformation(
+            @PathVariable Long memberId
+    ) {
+        MemberInformationResDTO data = memberAdminUsecase.readMemberInformation(memberId);
+        return ApiResponse.response(HttpStatus.OK, MEMBER_INFORMATION_READ.getMessage(), data);
     }
 
 }
