460dbfe90a90f30e3639244586f1c2b5f9951827
koreaioi <koreaioi@gachon.ac.kr>
2026-01-18 20:12:01 +0900

    feat: 가입신청 목록 API 구현

--- src/main/java/com/tavemakers/surf/domain/member/controller/AdminController.java
+++ src/main/java/com/tavemakers/surf/domain/member/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.tavemakers.surf.domain.member.dto.request.AdminPageLoginReqDto;
 import com.tavemakers.surf.domain.member.dto.request.PasswordReqDto;
 import com.tavemakers.surf.domain.member.dto.request.RoleChangeRequestDto;
 import com.tavemakers.surf.domain.member.dto.response.AdminPageLoginResDto;
+import com.tavemakers.surf.domain.member.dto.response.MemberRegistrationSliceResDTO;
 import com.tavemakers.surf.domain.member.usecase.MemberAdminUsecase;
 import io.swagger.v3.oas.annotations.Operation;
 import com.tavemakers.surf.global.common.response.ApiResponse;
@@ -14,8 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import static com.tavemakers.surf.domain.member.controller.ResponseMessage.ADMIN_PAGE_LOGIN_SUCCESS;
-import static com.tavemakers.surf.domain.member.controller.ResponseMessage.MANAGER_PASSWORD_SET_UP_SUCCESS;
+import static com.tavemakers.surf.domain.member.controller.ResponseMessage.*;
 
 @Tag(name = "관리자", description = "관리자용 API")
 @RestController
@@ -50,6 +50,16 @@ public class AdminController {
     ) {
         AdminPageLoginResDto data = memberAdminUsecase.loginAdminHomePage(dto, response);
         return ApiResponse.response(HttpStatus.OK, ADMIN_PAGE_LOGIN_SUCCESS.getMessage(),data);
+    }
+
+    @Operation(summary = "가입신청 목록", description = "가입신청 목록을 조회합니다.")
+    @GetMapping("/v1/manager/registration-list")
+    public ApiResponse<MemberRegistrationSliceResDTO> readRegistrationList (
+            @RequestParam int pageSize,
+            @RequestParam int pageNum
+    ) {
+        MemberRegistrationSliceResDTO data = memberAdminUsecase.readRegistrationList(pageSize, pageNum);
+        return ApiResponse.response(HttpStatus.OK, REGISTRATION_LIST_READ.getMessage(), data);
     }
 
 }
