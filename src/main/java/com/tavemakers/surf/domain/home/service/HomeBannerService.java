77d29bd2165ef86d96741efb019ff47ecc086d7a
tjdgus3488 <tjdgus3488@naver.com>
2026-02-01 19:32:17 +0900

    feat: 홈 배너 게시 상태 변경 api 작성

--- src/main/java/com/tavemakers/surf/domain/home/service/HomeBannerService.java
+++ src/main/java/com/tavemakers/surf/domain/home/service/HomeBannerService.java
@@ -133,4 +133,23 @@ public class HomeBannerService {
 
         return banners;
     }
+
+    @Transactional
+    public HomeBannerResDTO activateBanner(Long bannerId) {
+        HomeBanner banner = findBanner(bannerId);
+        banner.activate();
+        return HomeBannerResDTO.from(banner);
+    }
+
+    @Transactional
+    public HomeBannerResDTO deactivateBanner(Long bannerId) {
+        HomeBanner banner = findBanner(bannerId);
+        banner.deactivate();
+        return HomeBannerResDTO.from(banner);
+    }
+
+    private HomeBanner findBanner(Long id) {
+        return homeBannerRepository.findById(id)
+                .orElseThrow(HomeBannerNotFoundException::new);
+    }
 }
